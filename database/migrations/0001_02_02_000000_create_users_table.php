40a587a5f83a73072d2d86eed046bb405cc9973a
Leandro <diegolyt29@gmail.com>
2025-09-21 01:38:29 -0500

    Implemectación de las migraciones (#1)

---
+++ database/migrations/0001_02_02_000000_create_users_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_rol')
+                    ->constrained('rols');
+            $table->string('primer_nombre',50);
+            $table->string('segundo_nombre',50)->nullable();
+            $table->string('primer_apellido',50);
+            $table->string('segundo_apellido',50)->nullable();
+            $table->string('telefono',10);
+            $table->string('estado_cuenta')->default('activa');
+            $table->string('correo')->unique();
+            $table->string('contraseña_hash');
+            $table->string('cuenta_bancaria',30);  
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
---
+++ database/migrations/2025_09_20_100026_create_product_selects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_selects', function (Blueprint $table) {
+            $table->foreignId('id_producto')
+                    ->constrained('products')
+                    ->onDelete('cascade');
+            $table->foreignId('id_carrito')
+                    ->constrained('carts')
+                    ->onDelete('cascade');
+            $table->decimal('precio_unidad',10,2);
+            $table->unsignedInteger('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_selects');
+    }
+};
